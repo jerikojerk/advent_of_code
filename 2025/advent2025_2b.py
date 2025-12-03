1108f0e59588eef9107243a5786dddd1c6dc47c3
Jerikojerk <jerikojerk@gmail.com>
2025-12-03 22:38:35 +0100

    code cleanup

--- 2025/advent2025_2b.py
+++ 2025/advent2025_2b.py
@@ -5,59 +5,25 @@ from math import log10
 
 import re 
 
-INI_FILE="2025/advent2025_2-input.txt"
-def is_odd(x): return x % 2 == 1
+INI_FILE="2025/advent2025_2-ex.txt"
 
-def rangeBoundary_spliter( rangeBoundary:str ):
-    length=len(rangeBoundary)
-    firsthalf = length//2 
-    if firsthalf == 0:
-        print(f"strange boundary {rangeBoundary}")
-        return ( "1",rangeBoundary,1)
-    secondhalf=length-firsthalf
-    return ( rangeBoundary[:firsthalf], 
-        rangeBoundary[-secondhalf:],secondhalf)
+
 
 def range_analysis( range:str ):
-    pattern = re.compile(r"^([0-9]+)(\1)+$") 
+    pattern = re.compile(r"^([0-9]+)\1+$") 
     results=[]
     (bottom,top) = range.split("-",2)
-    bo = int(bottom)
-    to = int(top)
-    if  bo > to :
+    bot = int(bottom)
+    top = int(top)
+    if  bot > top :
         raise ValueError(f"Please verify  {range=} {bo=} > {to=}")
-    
-    (bleft,bright,bexp)=rangeBoundary_spliter(bottom)
-    #(tleft,tright,texp)=rangeBoundary_spliter(top)
-    candidat = bo
+    candidat = bot
     print(f"iterator initialisation {candidat=}")
-    while( True  ):
-        #print(f"{candidat=}")
-        if  to < candidat : 
-            break
-       
-        if bo <= candidat:
-            s = str(candidat)
-            if pattern.findall(s):
-                #print(f"success {candidat=} ")
-                results.append(candidat)
-            #else:
-                #print(f"last minute : erreur {s}")
-        #else candidat trop petit 
-
+    while( top > candidat  ):
+        s = str(candidat)
+        if pattern.findall(s):
+            results.append(candidat)
         candidat+=1
-        """
-        if   iterator >= max_exp or iterator < min_exp :
-            print(f"{candidat=}")
-            tmp = f"{min_exp=} {max_exp=} {iterator=}"
-            #there is a rule for no leading 0 
-            iterator=10**(exposant)
-            min_exp=max_exp
-            exposant+=1
-            max_exp = 10**exposant
-            print(f"iterator adjustment {tmp} => {min_exp=} {max_exp=} {iterator=}")
-        """
-    #endwhile    
     return results
 
 
